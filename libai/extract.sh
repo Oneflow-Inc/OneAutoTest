ae8462c1a469e75232d92e0f48fdecc33c14e799
Yu Yuqing <77087768+Yu-Yuqing@users.noreply.github.com>
2022-07-25 17:24:20 +0800

    refine extract script
    refine

--- libai/extract.sh
+++ libai/extract.sh
@@ -6,8 +6,8 @@ MEGATRON_LOG=extract_log/megatron_log
 mkdir -p $LIBAI_LOG
 mkdir -p $MEGATRON_LOG
 
-/path/to/ossutil64 -c /path/to/ossutilconfig cp -r -f $LIBAI_LOG/dlperf_result.md oss://oneflow-test/OneFlowAutoTest/huoshanyingqin/$COMMIT/ oss://oneflow-test/OneFlowAutoTest/huoshanyingqin/baseline/megatron_base_supple/full/ ./$MEGATRON_LOG/
-/path/to/ossutil64 -c /path/to/ossutilconfig cp -r -f $LIBAI_LOG/dlperf_result.md oss://oneflow-test/OneFlowAutoTest/huoshanyingqin/$COMMIT/ oss://oneflow-test/OneFlowAutoTest/huoshanyingqin/$COMMIT/ ./$LIBAI_LOG/
+/path/to/ossutil64 -c /path/to/ossutilconfig cp -r -f oss://oneflow-test/OneFlowAutoTest/huoshanyingqin/baseline/megatron_base_supple/full/ ./$MEGATRON_LOG/
+/path/to/ossutil64 -c /path/to/ossutilconfig cp -r -f oss://oneflow-test/OneFlowAutoTest/huoshanyingqin/$COMMIT/ ./$LIBAI_LOG/
 
 python3 extract_bert.py --test-log $LIBAI_LOG --compare-log $MEGATRON_LOG --oneflow-commit $COMMIT
 
