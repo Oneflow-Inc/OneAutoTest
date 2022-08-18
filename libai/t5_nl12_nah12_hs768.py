5c9e4dc656f4042e40c8e55e12613c9a29925036
Yongning Xu <809257291@qq.com>
2022-08-18 10:34:12 +0000

    update t5_nl12 configs

--- libai/t5_nl12_nah12_hs768.py
+++ libai/t5_nl12_nah12_hs768.py
@@ -11,14 +11,12 @@ from .common.data.t5_dataset import dataloader, tokenization
 
 from .common.models.graph import graph
 
-vocab_file = "/home/ylkj/dataset/bert-base-chinese-vocab.txt"
-data_prefix = "/home/ylkj/dataset/loss_compara_content_sentence"
+vocab_file = "/path/to/bert-base-chinese-vocab.txt"
+data_prefix = "/path/to/loss_compara_content_sentence"
 
 tokenization.tokenizer.vocab_file = vocab_file
 dataloader.train.dataset[0].data_prefix = data_prefix
 dataloader.train.dataset[0].indexed_dataset.data_prefix = data_prefix
-
-train.eval_iter = 10
 
 # Set all dropout to 0.
 model.cfg.hidden_dropout_prob = 0.1
@@ -28,7 +26,7 @@ model.cfg.bias_gelu_fusion = True
 model.cfg.bias_dropout_fusion = True
 
 # Set matched model arguments
-model.cfg.hidden_layers = 12
+#model.cfg.hidden_layers = 12
 model.cfg.hidden_size = 768
 model.cfg.intermediate_size = 3072
 model.cfg.num_attention_heads = 12
@@ -39,12 +37,14 @@ train.warmup_ratio = 0.01
 train.test_micro_batch_size = 4
 
 
-train.dist.pipeline_num_layers = 2 * model.cfg.hidden_layers
+#train.dist.pipeline_num_layers = 2 * model.cfg.hidden_layers
+
+train.input_placement_device = "cpu"
 
 # Set a constant lr scheduler after warmup
 optim.lr = 0.0001
 
 train.evaluation.evaluator = LazyCall(PPLEvaluator)()
 
-train.evaluation.enabled = True
+train.evaluation.enabled = False
 train.evaluation.eval_iter = 30
