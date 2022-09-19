0f7ea39b1120060b017ee0c2d9e46803543ec0d5
Yongning Xu <809257291@qq.com>
2022-09-19 08:52:08 +0000

    refine sed

--- libai/accuracy_verify/init.sh
+++ libai/accuracy_verify/init.sh
@@ -8,7 +8,7 @@ pip install -e .
 ## bert
 #  1n1g acc4        bert_nl24_nah16_hs1024_fp16_actrue_mp1_pp1_mb32_gb128_1n1g
 bash tools/args_libai_bert_init.sh configs/bert_nl24_nah16_hs1024.py 1 1 0 127.0.0.1 1 1 true true 32 128
-<<Long
+
 export CUDA_VISIBLE_DEVICES=0,1,4,5
 #  1n4g dp4 acc4        bert_nl24_nah16_hs1024_fp16_actrue_mp1_pp1_mb32_gb512_1n4g
 bash tools/args_libai_bert_init.sh configs/bert_nl24_nah16_hs1024.py 1 4 0 127.0.0.1 1 1 true true 32 512
@@ -69,4 +69,4 @@ bash tools/args_libai_t5_init.sh configs/t5_nl12_nah12_hs768.py 1 4 0 127.0.0.1 
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 #  1n8g 3-D acc4  t5_nl12_nah12_hs768_fp16_actrue_mp2_pp2_mb64_gb512_1n8g
 bash tools/args_libai_t5_init.sh configs/t5_nl12_nah12_hs768.py 1 8 0 127.0.0.1 2 2 true true 64 512
-Long
+
--- libai/accuracy_verify/run.sh
+++ libai/accuracy_verify/run.sh
@@ -7,18 +7,19 @@ sed -i 's/\/path\/to/\/ssd\/dataset\/libai_dataset/g' ./libai/configs/t5_nl12_na
 cp init.sh loss.sh draw_loss.py compose.py ./libai/
 cd libai && mkdir loss_txt && mkdir curve
 
-sed -i '/for self.iter in range(start_iter, max_iter):/a\                    if self.iter == 1:\n \
-                      cmd = "nvidia-smi --query-gpu=timestamp,name,driver_version,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv"\n \
+sed -i '/for self.iter in range(start_iter, max_iter):/a\                    if self.iter == 1: \
+                      cmd = "nvidia-smi --query-gpu=timestamp,name,driver_version,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv" \
                       os.system(cmd)' ./libai/engine/trainer.py
 sed -i '/import time/a\import os' ./libai/engine/trainer.py
 
 bash init.sh
 
+cd libai
 sed -i '/99/1' ./libai/engine/trainer.py
 sed -i '/hooks.PeriodicCheckpointer/#hooks.PeriodicCheckpointer' ./libai/engine/default.py
 sed -i '/persistent_workers=True/#persistent_workers=True' ./libai/data/build.py
 sed -i '/shuffle=True/shuffle=False' ./libai/data/build.py
-sed -i '/total_losses_reduced = sum(metrics_dict.values())/a\            if dist.is_main_process():\n \
-                txt = open("loss_txt/your_loss.txt", "a")\n \
-                txt.write(str(total_losses_reduced.item())+"\n")' ./libai/engine/trainer.py
+sed -i '/total_losses_reduced = sum(metrics_dict.values())/a\            if dist.is_main_process(): \
+                txt = open("loss_txt/your_loss.txt", "a") \
+                txt.write(str(total_losses_reduced.item())+"\\n")' ./libai/engine/trainer.py
 bash loss.sh
