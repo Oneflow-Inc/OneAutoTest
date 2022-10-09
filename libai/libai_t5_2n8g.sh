997961f2a9ecc283154cd3e906f8e8d99c8fd892
Yongning Xu <809257291@qq.com>
2022-10-09 08:14:30 +0000

    modify t5 model size to nl12

--- libai/libai_t5_2n8g.sh
+++ libai/libai_t5_2n8g.sh
@@ -5,25 +5,25 @@ NODE_RANK=$MLP_ROLE_INDEX
 MASTER_ADDR=$MLP_WORKER_0_HOST
 
 #  2n8g 数据并行        t5_nl24_nah16_hs1024_fp16_acfalse_mp1_pp1_mb4_gb64_2n8g
-bash tools/args_libai_t5.sh configs/t5_nl24_nah16_hs2304.py 2 8 $NODE_RANK $MASTER_ADDR 1 1 true false 4 64
+bash tools/args_libai_t5_nl24.sh configs/t5_nl24_nah16_hs2304.py 2 8 $NODE_RANK $MASTER_ADDR 1 1 true false 4 64
 
 #  2n8g 数据并行        t5_nl24_nah16_hs1024_fp16_actrue_mp1_pp1_mb32_gb2048_2n8g
-bash tools/args_libai_t5.sh configs/t5_nl24_nah16_hs2304.py 2 8 $NODE_RANK $MASTER_ADDR 1 1 true true 32 2048
+bash tools/args_libai_t5_nl24.sh configs/t5_nl24_nah16_hs2304.py 2 8 $NODE_RANK $MASTER_ADDR 1 1 true true 32 2048
 
 #  2n8g 数据+模型并行   t5_nl24_nah16_hs1024_fp16_actrue_mp4_pp1_mb32_gb512_2n8g
-bash tools/args_libai_t5.sh configs/t5_nl24_nah16_hs2304.py 2 8 $NODE_RANK $MASTER_ADDR 4 1 true true 32 512
+bash tools/args_libai_t5_nl24.sh configs/t5_nl24_nah16_hs2304.py 2 8 $NODE_RANK $MASTER_ADDR 4 1 true true 32 512
 
 #  2n8g 数据+模型并行   t5_nl24_nah16_hs1024_fp16_actrue_mp2_pp1_mb32_gb2048_2n8g
-bash tools/args_libai_t5.sh configs/t5_nl24_nah16_hs2304.py 2 8 $NODE_RANK $MASTER_ADDR 2 1 true true 32 2048
+bash tools/args_libai_t5_nl24.sh configs/t5_nl24_nah16_hs2304.py 2 8 $NODE_RANK $MASTER_ADDR 2 1 true true 32 2048
 
 #  2n8g 数据+模型并行   t5_nl24_nah16_hs1024_fp16_actrue_mp8_pp1_mb32_gb512_2n8g
-bash tools/args_libai_t5.sh configs/t5_nl24_nah16_hs2304.py 2 8 $NODE_RANK $MASTER_ADDR 8 1 true true 32 512
+bash tools/args_libai_t5_nl24.sh configs/t5_nl24_nah16_hs2304.py 2 8 $NODE_RANK $MASTER_ADDR 8 1 true true 32 512
 
 #  2n8g 数据+流水并行   t5_nl24_nah16_hs1024_fp16_actrue_mp1_pp4_mb32_gb1024_2n8g
-bash tools/args_libai_t5.sh configs/t5_nl24_nah16_hs2304.py 2 8 $NODE_RANK $MASTER_ADDR 1 4 true true 32 1024
+bash tools/args_libai_t5_nl24.sh configs/t5_nl24_nah16_hs2304.py 2 8 $NODE_RANK $MASTER_ADDR 1 4 true true 32 1024
 
 #  2n8g 3-D并行 t5_nl24_nah16_hs1024_fp16_actrue_mp2_pp4_mb32_gb512_2n8g
-bash tools/args_libai_t5.sh configs/t5_nl24_nah16_hs2304.py 2 8 $NODE_RANK $MASTER_ADDR 2 4 true true 32 512
+bash tools/args_libai_t5_nl24.sh configs/t5_nl24_nah16_hs2304.py 2 8 $NODE_RANK $MASTER_ADDR 2 4 true true 32 512
 
 #  2n8g 3-D并行 t5_nl24_nah16_hs1024_fp16_actrue_mp2_pp4_mb32_gb1024_2n8g
-bash tools/args_libai_t5.sh configs/t5_nl24_nah16_hs2304.py 2 8 $NODE_RANK $MASTER_ADDR 2 4 true true 32 1024
+bash tools/args_libai_t5_nl24.sh configs/t5_nl24_nah16_hs2304.py 2 8 $NODE_RANK $MASTER_ADDR 2 4 true true 32 1024
