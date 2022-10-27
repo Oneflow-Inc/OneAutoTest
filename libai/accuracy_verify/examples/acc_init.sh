ef69367dfcbfbe437ee92050ee9190047b20e209
Yongning Xu <809257291@qq.com>
2022-10-27 03:09:31 +0000

    add jupyter setup

--- libai/accuracy_verify/examples/acc_init.sh
+++ libai/accuracy_verify/examples/acc_init.sh
@@ -3,7 +3,6 @@
 #  1n1g acc4        bert_nl24_nah16_hs1024_fp16_actrue_mp1_pp1_mb32_gb128_1n1g
 bash tools/args_libai_bert_init.sh configs/bert_nl24_nah16_hs1024.py 1 1 0 127.0.0.1 1 1 true true 32 128
 
-export CUDA_VISIBLE_DEVICES=0,1,4,5
 #  1n4g dp4 acc4        bert_nl24_nah16_hs1024_fp16_actrue_mp1_pp1_mb32_gb512_1n4g
 bash tools/args_libai_bert_init.sh configs/bert_nl24_nah16_hs1024.py 1 4 0 127.0.0.1 1 1 true true 32 512
 
@@ -16,7 +15,6 @@ bash tools/args_libai_bert_init.sh configs/bert_nl24_nah16_hs1024.py 1 4 0 127.0
 #  1n4g 2-D 模型+流水并行 acc4  bert_nl24_nah16_hs1024_fp16_actrue_mp2_pp2_mb64_gb256_1n4g
 bash tools/args_libai_bert_init.sh configs/bert_nl24_nah16_hs1024.py 1 4 0 127.0.0.1 2 2 true true 64 256
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 #  1n8g 3-D acc4  bert_nl24_nah16_hs1024_fp16_actrue_mp2_pp2_mb64_gb512_1n8g
 bash tools/args_libai_bert_init.sh configs/bert_nl24_nah16_hs1024.py 1 8 0 127.0.0.1 2 2 true true 64 512
 
@@ -25,7 +23,6 @@ bash tools/args_libai_bert_init.sh configs/bert_nl24_nah16_hs1024.py 1 8 0 127.0
 #  1n1g acc4        gpt2_nl24_nah16_hs1024_fp16_actrue_mp1_pp1_mb8_gb32_1n1g
 #bash tools/args_libai_gpt2_init.sh configs/gpt2_nl24_nah16_hs1024.py 1 1 0 127.0.0.1 1 1 true true 8 32
 
-export CUDA_VISIBLE_DEVICES=0,1,4,5
 #  1n4g dp4 acc4        gpt2_nl24_nah16_hs1024_fp16_actrue_mp1_pp1_mb4_gb64_1n4g
 bash tools/args_libai_gpt2_init.sh configs/gpt2_nl24_nah16_hs1024.py 1 4 0 127.0.0.1 1 1 true true 4 64
 
@@ -38,7 +35,6 @@ bash tools/args_libai_gpt2_init.sh configs/gpt2_nl24_nah16_hs1024.py 1 4 0 127.0
 #  1n4g 2-D 模型+流水并行 acc4  gpt2_nl24_nah16_hs1024_fp16_actrue_mp2_pp2_mb16_gb64_1n4g
 bash tools/args_libai_gpt2_init.sh configs/gpt2_nl24_nah16_hs1024.py 1 4 0 127.0.0.1 2 2 true true 16 64
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 #  1n8g 3-D acc4  gpt2_nl24_nah16_hs1024_fp16_actrue_mp2_pp2_mb16_gb128_1n8g
 bash tools/args_libai_gpt2_init.sh configs/gpt2_nl24_nah16_hs1024.py 1 8 0 127.0.0.1 2 2 true true 16 128
 
@@ -47,7 +43,6 @@ bash tools/args_libai_gpt2_init.sh configs/gpt2_nl24_nah16_hs1024.py 1 8 0 127.0
 #  1n1g acc4        t5_nl12_nah12_hs768_fp16_actrue_mp1_pp1_mb32_gb128_1n1g
 bash tools/args_libai_t5_init.sh configs/t5_nl12_nah12_hs768.py 1 1 0 127.0.0.1 1 1 true true 32 128
 
-export CUDA_VISIBLE_DEVICES=0,1,4,5
 #  1n4g dp4 acc4        t5_nl12_nah12_hs768_fp16_actrue_mp1_pp1_mb16_gb256_1n4g
 bash tools/args_libai_t5_init.sh configs/t5_nl12_nah12_hs768.py 1 4 0 127.0.0.1 1 1 true true 16 256
 
@@ -60,6 +55,5 @@ bash tools/args_libai_t5_init.sh configs/t5_nl12_nah12_hs768.py 1 4 0 127.0.0.1 
 #  1n4g 2-D 模型+流水并行 acc4  t5_nl12_nah12_hs768_fp16_actrue_mp2_pp2_mb64_gb256_1n4g
 bash tools/args_libai_t5_init.sh configs/t5_nl12_nah12_hs768.py 1 4 0 127.0.0.1 2 2 true true 64 256
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 #  1n8g 3-D acc4  t5_nl12_nah12_hs768_fp16_actrue_mp2_pp2_mb64_gb512_1n8g
 bash tools/args_libai_t5_init.sh configs/t5_nl12_nah12_hs768.py 1 8 0 127.0.0.1 2 2 true true 64 512
--- libai/accuracy_verify/examples/acc_loss.sh
+++ libai/accuracy_verify/examples/acc_loss.sh
@@ -5,7 +5,6 @@ COMMIT=${1:-"master"}
 #  1n1g acc4        bert_nl24_nah16_hs1024_fp16_actrue_mp1_pp1_mb32_gb128_1n1g
 bash tools/args_libai_bert_loss.sh configs/bert_nl24_nah16_hs1024.py 1 1 0 127.0.0.1 1 1 true true 32 128 ${COMMIT}
 
-export CUDA_VISIBLE_DEVICES=0,1,4,5
 #  1n4g dp4 acc4        bert_nl24_nah16_hs1024_fp16_actrue_mp1_pp1_mb32_gb512_1n4g
 bash tools/args_libai_bert_loss.sh configs/bert_nl24_nah16_hs1024.py 1 4 0 127.0.0.1 1 1 true true 32 512 ${COMMIT}
 
@@ -18,7 +17,6 @@ bash tools/args_libai_bert_loss.sh configs/bert_nl24_nah16_hs1024.py 1 4 0 127.0
 #  1n4g 2-D 模型+流水并行 acc4  bert_nl24_nah16_hs1024_fp16_actrue_mp2_pp2_mb64_gb256_1n4g
 bash tools/args_libai_bert_loss.sh configs/bert_nl24_nah16_hs1024.py 1 4 0 127.0.0.1 2 2 true true 64 256 ${COMMIT}
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 #  1n8g 3-D acc4  bert_nl24_nah16_hs1024_fp16_actrue_mp2_pp2_mb64_gb512_1n8g
 bash tools/args_libai_bert_loss.sh configs/bert_nl24_nah16_hs1024.py 1 8 0 127.0.0.1 2 2 true true 64 512 ${COMMIT}
 
@@ -27,7 +25,6 @@ bash tools/args_libai_bert_loss.sh configs/bert_nl24_nah16_hs1024.py 1 8 0 127.0
 #  1n1g acc4        gpt2_nl24_nah16_hs1024_fp16_actrue_mp1_pp1_mb8_gb32_1n1g
 #bash tools/args_libai_gpt2_loss.sh configs/gpt2_nl24_nah16_hs1024.py 1 1 0 127.0.0.1 1 1 true true 8 32 ${COMMIT}
 
-export CUDA_VISIBLE_DEVICES=0,1,4,5
 #  1n4g dp4 acc4        gpt2_nl24_nah16_hs1024_fp16_actrue_mp1_pp1_mb4_gb64_1n4g
 bash tools/args_libai_gpt2_loss.sh configs/gpt2_nl24_nah16_hs1024.py 1 4 0 127.0.0.1 1 1 true true 4 64 ${COMMIT}
 
@@ -40,7 +37,6 @@ bash tools/args_libai_gpt2_loss.sh configs/gpt2_nl24_nah16_hs1024.py 1 4 0 127.0
 #  1n4g 2-D 模型+流水并行 acc4  gpt2_nl24_nah16_hs1024_fp16_actrue_mp2_pp2_mb16_gb64_1n4g
 bash tools/args_libai_gpt2_loss.sh configs/gpt2_nl24_nah16_hs1024.py 1 4 0 127.0.0.1 2 2 true true 16 64 ${COMMIT}
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 #  1n8g 3-D acc4  gpt2_nl24_nah16_hs1024_fp16_actrue_mp2_pp2_mb16_gb128_1n8g
 bash tools/args_libai_gpt2_loss.sh configs/gpt2_nl24_nah16_hs1024.py 1 8 0 127.0.0.1 2 2 true true 16 128 ${COMMIT}
 
@@ -49,7 +45,6 @@ bash tools/args_libai_gpt2_loss.sh configs/gpt2_nl24_nah16_hs1024.py 1 8 0 127.0
 #  1n1g acc4        t5_nl12_nah12_hs768_fp16_actrue_mp1_pp1_mb32_gb128_1n1g
 bash tools/args_libai_t5_loss.sh configs/t5_nl12_nah12_hs768.py 1 1 0 127.0.0.1 1 1 true true 32 128 ${COMMIT}
 
-export CUDA_VISIBLE_DEVICES=0,1,4,5
 #  1n4g dp4 acc4        t5_nl12_nah12_hs768_fp16_actrue_mp1_pp1_mb16_gb256_1n4g
 bash tools/args_libai_t5_loss.sh configs/t5_nl12_nah12_hs768.py 1 4 0 127.0.0.1 1 1 true true 16 256 ${COMMIT}
 
@@ -62,6 +57,5 @@ bash tools/args_libai_t5_loss.sh configs/t5_nl12_nah12_hs768.py 1 4 0 127.0.0.1 
 #  1n4g 2-D 模型+流水并行 acc4  t5_nl12_nah12_hs768_fp16_actrue_mp2_pp2_mb64_gb256_1n4g
 bash tools/args_libai_t5_loss.sh configs/t5_nl12_nah12_hs768.py 1 4 0 127.0.0.1 2 2 true true 64 256 ${COMMIT}
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 #  1n8g 3-D acc4  t5_nl12_nah12_hs768_fp16_actrue_mp2_pp2_mb64_gb512_1n8g
 bash tools/args_libai_t5_loss.sh configs/t5_nl12_nah12_hs768.py 1 8 0 127.0.0.1 2 2 true true 64 512 ${COMMIT}
