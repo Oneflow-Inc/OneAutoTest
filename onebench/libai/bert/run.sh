10cb7c32db00de4e95944f057692105b60df62c5
ouyangyu <xuanjiuye@gmail.com>
2022-12-22 08:21:08 +0000

    bert 3080ti

--- onebench/libai/bert/run.sh
+++ onebench/libai/bert/run.sh
@@ -42,10 +42,76 @@ python3 -m pip install -e . --user
 #       train_iter log_period
 #       hidden_layers num_attention_heads hidden_size intermediate_size head_size 
 
-bash tools/args_train.sh configs/bert_large_pretrain.py 1 1 0 127.0.0.1 \
-1 1 false false false \
-1 1 false 0 \
-220 100 \
-24 16 1024 4096
+# eager test
+# bash tools/args_train.sh configs/bert_large_pretrain.py 1 1 0 127.0.0.1 1 1 false false false 1 1 false 0 220 100 24 16 1024 4096
+
+## BERT
+# 3080ti  
+#  1n1g         bert_nl24_nah16_hs1024_fp16_acfalse_mp1_pp1_mb6_gb6_1n1g
+bash tools/args_train.sh configs/bert_large_pretrain.py 1 1 0 127.0.0.1 1 1 true true false 4 4 false 0 220 100 24 16 1024 4096
+
+#  1n1g         bert_nl24_nah16_hs1024_fp16_actrue_mp1_pp1_mb32_gb256_1n1g
+bash tools/args_train.sh configs/bert_large_pretrain.py 1 1 0 127.0.0.1 1 1 true true true true 16 128 false 0 220 100 24 16 1024 4096
+
+#  1n1g         bert_nl24_nah16_hs1024_fp16_actrue_mp1_pp1_mb32_gb32_1n1g
+bash tools/args_train.sh configs/bert_large_pretrain.py 1 1 0 127.0.0.1 1 1 true true true 32 32 false 0 220 100 24 16 1024 4096
 
 
+export CUDA_VISIBLE_DEVICES=0,1,4,5
+#  1n4g 模型并行        bert_nl24_nah16_hs1024_fp16_acfalse_mp4_pp1_mb6_gb6_1n4g
+bash tools/args_train.sh configs/bert_large_pretrain.py 1 4 0 127.0.0.1 4 1 true true false 6 6 false 0 220 100 24 16 1024 4096
+
+#  1n4g 模型并行        bert_nl24_nah16_hs1024_fp16_actrue_mp4_pp1_mb32_gb256_1n4g
+bash tools/args_train.sh configs/bert_large_pretrain.py 1 4 0 127.0.0.1 4 1 true true true 32 256 false 0 220 100 24 16 1024 4096
+
+#  1n4g 数据并行        bert_nl24_nah16_hs1024_fp16_acfalse_mp1_pp1_mb4_gb16_1n4g
+bash tools/args_train.sh configs/bert_large_pretrain.py 1 4 0 127.0.0.1 1 1 true true false 4 16 false 0 220 100 24 16 1024 4096
+
+#  1n4g 数据并行        bert_nl24_nah16_hs1024_fp16_actrue_mp1_pp1_mb32_gb128_1n4g
+bash tools/args_train.sh configs/bert_large_pretrain.py 1 4 0 127.0.0.1 1 1 true true true 32 128 false 0 220 100 24 16 1024 4096
+
+
+#  1n4g 流水并行        bert_nl24_nah16_hs1024_fp16_actrue_mp1_pp4_mb32_gb256_1n4g
+bash tools/args_train.sh configs/bert_large_pretrain.py 1 4 0 127.0.0.1 1 4 true true true 32 256 false 0 220 100 24 16 1024 4096
+
+#  1n4g 流水并行        bert_nl24_nah16_hs1024_fp16_actrue_mp1_pp4_mb48_gb384_1n4g
+bash tools/args_train.sh configs/bert_large_pretrain.py 1 4 0 127.0.0.1 1 4 true true true 48 384 false 0 220 100 24 16 1024 4096
+
+#  1n4g 流水并行        bert_nl48_nah16_hs1024_fp16_actrue_mp1_pp4_mb16_gb128_1n4g
+bash tools/args_train.sh configs/bert_large_pretrain.py 1 4 0 127.0.0.1 1 4 true true true 16 128 false 0 220 100 48 16 1024 4096
+
+
+#  1n4g 数据+模型并行   bert_nl24_nah16_hs1024_fp16_actrue_mp2_pp1_mb32_gb512_1n4g
+bash tools/args_train.sh configs/bert_large_pretrain.py 1 4 0 127.0.0.1 2 1 true true true 32 512 false 0 220 100 24 16 1024 4096
+
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+#  1n8g 模型并行        bert_nl24_nah16_hs1024_fp16_acfalse_mp8_pp1_mb32_gb32_1n8g
+bash tools/args_train.sh configs/bert_large_pretrain.py 1 8 0 127.0.0.1 8 1 true true false 32 32 false 0 220 100 24 16 1024 4096
+
+#  1n8g 模型并行        bert_nl24_nah16_hs1024_fp16_actrue_mp8_pp1_mb32_gb256_1n8g
+bash tools/args_train.sh configs/bert_large_pretrain.py 1 8 0 127.0.0.1 8 1 true true true 32 256 false 0 220 100 24 16 1024 4096
+
+#  1n8g 流水并行        bert_nl48_nah16_hs1024_fp16_actrue_mp1_pp8_mb16_gb256_1n8g
+bash tools/args_train.sh configs/bert_large_pretrain.py 1 8 0 127.0.0.1 1 8 true true true 16 256 false 0 220 100 48 16 1024 4096
+
+#  1n8g 流水并行        bert_nl48_nah16_hs1024_fp16_actrue_mp1_pp8_mb24_gb384_1n8g
+bash tools/args_train.sh configs/bert_large_pretrain.py 1 8 0 127.0.0.1 1 8 true true true 24 384 false 0 220 100 48 16 1024 4096
+
+#  1n8g 数据并行        bert_nl24_nah16_hs1024_fp16_acfalse_mp1_pp1_mb4_gb32_1n8g
+bash tools/args_train.sh configs/bert_large_pretrain.py 1 8 0 127.0.0.1 1 1 true true false 4 32 false 0 220 100 24 16 1024 4096
+
+#  1n8g 数据并行        bert_nl24_nah16_hs1024_fp16_actrue_mp1_pp1_mb32_gb256_1n8g
+bash tools/args_train.sh configs/bert_large_pretrain.py 1 8 0 127.0.0.1 1 1 true true true 32 256 false 0 220 100 24 16 1024 4096
+
+#  1n8g 数据并行        bert_nl24_nah16_hs1024_fp16_actrue_mp1_pp1_mb32_gb2048_1n8g
+bash tools/args_train.sh configs/bert_large_pretrain.py 1 8 0 127.0.0.1 1 1 true true true 32 2048 false 0 220 100 24 16 1024 4096
+
+#  1n8g 数据+模型并行   bert_nl24_nah16_hs1024_fp16_actrue_mp2_pp1_mb32_gb1024_1n8g
+bash tools/args_train.sh configs/bert_large_pretrain.py 1 8 0 127.0.0.1 2 1 true true true 32 1024 false 0 220 100 24 16 1024 4096
+
+#  1n8g 数据+模型并行   bert_nl24_nah16_hs1024_fp16_actrue_mp2_pp1_mb32_gb128_1n8g
+bash tools/args_train.sh configs/bert_large_pretrain.py 1 8 0 127.0.0.1 2 1 true true true 32 128 false 0 220 100 24 16 1024 4096
+
+#  1n8g 数据+流水并行   bert_nl24_nah16_hs1024_fp16_actrue_mp1_pp4_mb32_gb512_1n8g
+bash tools/args_train.sh configs/bert_large_pretrain.py 1 8 0 127.0.0.1 1 4 true true true 32 512 false 0 220 100 24 16 1024 4096
