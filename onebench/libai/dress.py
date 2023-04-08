32373f78d00f653f8a3839f66b015a92c6de67bb
jinfengSUN <sjf1998112@gmail.com>
2023-04-08 14:29:09 +0800

    Update dress.py

--- onebench/libai/dress.py
+++ onebench/libai/dress.py
@@ -4,74 +4,52 @@ import json
 import os
 import click
 
+
 @click.command()
 @click.option('--path', prompt='Your path', help='Path of your floater')
+
 def print_loss(path):
     total_x = []
-    total_y = []
+    master_loss = []
+    naive_loss = []
+    rank_loss = []
     name = ""
     for root, dirs, files in os.walk(path):
         if len(dirs) > 0 and dirs[-1].startswith('LibAI'):
             name = dirs[-1]
         for file in files:
-            
             if file.endswith('.json'):
-
-                try:
+                if "master" in root:
                     with open(os.path.join(root, file), 'r', encoding="utf-8", newline="\n") as f:
-                        com_x = []
-                        dev_y = []
                         for line in f.readlines():
                             dev_data = json.loads(line)
-                            
-                            dev_y.append(dev_data['total_loss'])
-                            com_x.append(dev_data["iteration"])
+                            total_x.append(dev_data["iteration"])
+                            master_loss.append(dev_data["total_loss"])
+                if "naive" in root:
+                    with open(os.path.join(root, file), 'r', encoding="utf-8", newline="\n") as f:
+                        for line in f.readlines():
+                            dev_data = json.loads(line)
+                            naive_loss.append(dev_data["total_loss"])
+                if "rank" in root:
+                    with open(os.path.join(root, file), 'r', encoding="utf-8", newline="\n") as f:
+                        for line in f.readlines():
+                            dev_data = json.loads(line)
+                            rank_loss.append(dev_data["total_loss"])
 
-                except Exception as e:
-                    print(f"Error opening file {os.path.join(root, file)}: {e}")
-        
-                total_y.append(dev_y)
-                total_x.append(com_x)
+    step_names = ["", "50-220", "100-220"]
+    step = [0, 50, 100]
+    for i, s in enumerate(step):
+        plt.figure(figsize=(10, 8), dpi=120)
+        plt.plot(total_x[s:], rank_loss[s:], label="rank_per_process")
+        plt.plot(total_x[s:], naive_loss[s:], label="naive", linestyle="--")
+        plt.plot(total_x[s:], master_loss[s:], label="master", linestyle=":")
 
-    new_total_y = []
-    new_total_x = []
-    for i in range(len(total_y)):
-        if len(total_y[i]) >= 20:
-            new_total_y.append(total_y[i])
-            new_total_x.append(total_x[i])
-
-    plt.figure(figsize=(10, 8), dpi=120)
-    for i in range(0, len(total_y), 3):
-        plt.plot(new_total_x[i],new_total_y[i],label="rank_per_process")
-        plt.plot(new_total_x[i + 1],new_total_y[i + 1],label="master",linestyle = "--")
-        plt.plot(new_total_x[i + 2],new_total_y[i + 2],label="naive",linestyle = ":")
         plt.xlabel("iteration")
         plt.ylabel("total_loss")
         plt.title(name[62:])
-        plt.legend()   #打上标签
-        plt.savefig("./" + name + ".png")
+        plt.legend()  # 打上标签
+        plt.savefig("./" + name + step_names[i] + ".png")
 
-    plt.figure(figsize=(10, 8), dpi=120)
-    for i in range(0, len(total_y), 3):
-        plt.plot(new_total_x[i][50:],new_total_y[i][50:],label="rank_per_process")
-        plt.plot(new_total_x[i + 1][50:],new_total_y[i + 1][50:],label="master",linestyle = "--")
-        plt.plot(new_total_x[i + 2][50:],new_total_y[i + 2][50:],label="naive",linestyle = ":")
-        plt.xlabel("iteration")
-        plt.ylabel("total_loss")
-        plt.title(name[62:])
-        plt.legend()   #打上标签
-        plt.savefig("./" + name + "_50-220.png")
-
-    plt.figure(figsize=(10, 8), dpi=120)
-    for i in range(0, len(total_y), 3):
-        plt.plot(new_total_x[i][100:],new_total_y[i][100:],label="rank_per_process")
-        plt.plot(new_total_x[i + 1][100:],new_total_y[i + 1][100:],label="master",linestyle = "--")
-        plt.plot(new_total_x[i + 2][100:],new_total_y[i + 2][100:],label="naive",linestyle = ":")
-        plt.xlabel("iteration")
-        plt.ylabel("total_loss")
-        plt.title(name[62:])
-        plt.legend()   #打上标签
-        plt.savefig("./" + name + "._100-220.png")
 
 if __name__ == '__main__':
     print_loss()
