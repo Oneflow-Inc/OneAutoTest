a4fb215535026fc89262a530bc8e0bb094a4149e
jinfengSUN <sjf1998112@gmail.com>
2023-04-28 17:48:42 +0800

    添加火山脚本

--- onebench/libai/run_megatron_ml_gpt.sh
+++ onebench/libai/run_megatron_ml_gpt.sh
@@ -16,11 +16,11 @@ wget -nc https://oneflow-test.oss-cn-beijing.aliyuncs.com/OneFlowAutoTest/libai/
 wget -nc https://oneflow-test.oss-cn-beijing.aliyuncs.com/OneFlowAutoTest/libai/dataset/loss_compara_content_sentence.idx  -P ./data_test/gpt_data
 
 
-wget https://github.com/Tendo33/OneAutoTest/raw/megatron_script/onebench/libai/megatron_args_pretrain_gpt2.sh -P ./examples
+wget https://github.com/Oneflow-Inc/OneAutoTest/raw/megatron_script_huoshan/onebench/libai/megatron_args_pretrain_gpt2_huoshan.sh -P ./examples
 
 
 #1n1g
-bash examples/megatron_args_pretrain_gpt2.sh 1 1 0 127.0.0.1 1 1 true true true 5 5 false 2 220 1 24 16 768 3072 64 true false
+bash examples/megatron_args_pretrain_gpt2.sh  1 1 true true true 6 6 false 2 220 1 24 16 768 3072 64 
 
 #1n8g
-bash examples/megatron_args_pretrain_gpt2.sh 1 8 0 127.0.0.1 8 1 true true true 32 256 false 2 220 1 24 16 768 3072 64 true false
+bash examples/megatron_args_pretrain_gpt2.sh  8 1 true true true 32 256 false 2 220 1 24 16 768 3072 64 
