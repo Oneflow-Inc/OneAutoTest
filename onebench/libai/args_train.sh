945c7af71fd6b165a98debf52694d73a53d48d47
jinfengSUN <sjf1998112@gmail.com>
2023-05-09 15:48:08 +0800

    Update args_train.sh

--- onebench/libai/args_train.sh
+++ onebench/libai/args_train.sh
@@ -33,10 +33,12 @@ HEAD_SIZE=${17:-64}
 
 ONEFLOW_COMMIT=$(python3 -c 'import oneflow; print(oneflow.__git_commit__)')
 
-sed -i '/import time/a\import os' ./libai/engine/trainer.py
-sed -i '/for self.iter in range(start_iter, max_iter):/a\                    if self.iter == 99: \
-                        cmd = "nvidia-smi --query-gpu=timestamp,name,driver_version,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv" \
-                        os.system(cmd)' ./libai/engine/trainer.py
+if [ $NODE_RANK -eq 0 ]; then
+  sed -i '/import time/a\import os' ./libai/engine/trainer.py
+  sed -i '/for self.iter in range(start_iter, max_iter):/a\                    if self.iter == 99: \
+                          cmd = "nvidia-smi --query-gpu=timestamp,name,driver_version,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv" \
+                          os.system(cmd)' ./libai/engine/trainer.py
+fi
 
 GPU_NAME="$(nvidia-smi -i 0 --query-gpu=gpu_name --format=csv,noheader)"
 GPU_NAME="${GPU_NAME// /_}"
@@ -128,6 +130,6 @@ echo "oneflow-libai(git_commit)=$ONEFLOW_LIBAI_COMMIT" >> ${LOG_FILENAME}/output
 
 
 
-if [ $NODE_INDEX -eq 0 ]; then
+if [ $NODE_RANK -eq 0 ]; then
     git checkout ./libai/engine/*.py
 fi
