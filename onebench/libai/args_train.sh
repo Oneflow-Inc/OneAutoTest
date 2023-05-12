3d6d9875eabe2e1efb582bdafacd4dc70c3d405d
SunJinfeng <86536994+Tendo33@users.noreply.github.com>
2023-05-12 11:23:48 +0800

    Update args_train.sh

--- onebench/libai/args_train.sh
+++ onebench/libai/args_train.sh
@@ -1,5 +1,13 @@
 set -ex
 
+export NCCL_IB_DISABLE=0
+export NCCL_DEBUG=INFO
+export NCCL_IB_GID_INDEX=3
+export NCCL_GDR_LEVEL=2
+# 安装 TCCL 之后不需要 NCCL TOPO 文件 comment 这行
+export NCCL_TOPO_FILE=/data_32T/home/workspace/nccl-tests/nccl_topo_a800_1.6t.xml
+export NCCL_IB_QPS_PER_CONNECTION=4
+
 CONFIG=$1
 NNODES=${2:-1}
 GPUS_PER_NODE=${3:-8}
