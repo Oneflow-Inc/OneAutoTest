a0d4bde9f7382c9cc60440d2f0bc0563f5669e73
SunJinfeng <86536994+Tendo33@users.noreply.github.com>
2023-05-12 11:24:01 +0800

    Update megatron_args_pretrain_gpt2.sh

--- onebench/libai/megatron_args_pretrain_gpt2.sh
+++ onebench/libai/megatron_args_pretrain_gpt2.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+export NCCL_IB_DISABLE=0
+export NCCL_DEBUG=INFO
+export NCCL_IB_GID_INDEX=3
+export NCCL_GDR_LEVEL=2
+# 安装 TCCL 之后不需要 NCCL TOPO 文件 comment 这行
+export NCCL_TOPO_FILE=/data_32T/home/workspace/nccl-tests/nccl_topo_a800_1.6t.xml
+export NCCL_IB_QPS_PER_CONNECTION=4
+
 # volcengine.com
 #export NCCL_IB_PCI_RELAXED_ORDERING=1
 NNODES=${1:-1}
