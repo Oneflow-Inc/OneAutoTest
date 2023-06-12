5df6dfb60f0103d2c5fcd56a09a58f940fad4d9a
jinfengSUN <sjf1998112@gmail.com>
2023-06-12 11:43:49 +0800

    add_script_platform

--- onebench/libai/args_train.sh
+++ onebench/libai/args_train.sh
@@ -25,7 +25,6 @@ HEAD_SIZE=${21:-64}
 SAVE_MODEL=${22:-false}
 UNSET_DROPOUT=${23:-false}
 
-
 ONEFLOW_COMMIT=$(python3 -c 'import oneflow; print(oneflow.__git_commit__)')
 
 sed -i '/import time/a\import os' ./libai/engine/trainer.py
@@ -39,7 +38,6 @@ TRAN_MODEL=${CONFIG##*/}
 TRAN_MODEL="LibAI_${TRAN_MODEL%*.py}"
 #RUN_TIME=$(date "+%Y%m%d_%H%M%S%N")
 
-
 AMP_OR="FP32"
 if $USE_FP16; then
     AMP_OR="FP16"
@@ -50,7 +48,7 @@ if $GRAPH_ENABLED; then
     RUN_TYPE="graph"
 fi
 
-# const 
+# const
 TRAIN_EPOCH=0
 LOAD_WEIGHT=""
 EVALUATION_ENABLED=false
@@ -58,13 +56,12 @@ hidden_dropout_prob=0.1
 attention_probs_dropout_prob=0.1
 bias_dropout_fusion=true
 
-DP=`expr $NNODES \* $GPUS_PER_NODE \/ $MP \/ $PP`
-ACC=`expr $GLOBAL_BATCH_SIZE \/ $DP \/ $MICRO_BATCH_SIZE`
+DP=$(expr $NNODES \* $GPUS_PER_NODE \/ $MP \/ $PP)
+ACC=$(expr $GLOBAL_BATCH_SIZE \/ $DP \/ $MICRO_BATCH_SIZE)
 
 LOG_FOLDER=test_logs/$HOSTNAME/${GPU_NAME}
 
 LOG_FILENAME=${TRAN_MODEL}_${RUN_TYPE}_nl${NUM_LAYER}_nah${NUM_ATT_HEADS}_hs${HIDDEN_SIZE}_${AMP_OR}_ac${ACTIVATION_CHECKPOINT}_DP${DP}_MP${MP}_PP${PP}_zero${ZERO_ENABLE}_stage${ZERO_STAGE}_mbs${MICRO_BATCH_SIZE}_gbs${GLOBAL_BATCH_SIZE}_acc${ACC}_${NNODES}n${GPUS_PER_NODE}g
-
 
 if [[ $UNSET_DROPOUT = "true" ]]; then
     #sed -i 's/persistent_workers=True/#persistent_workers=True/g' ./libai/data/build.py
@@ -93,41 +90,41 @@ echo LOG_FILENAME=$LOG_FILENAME
 
 # nsys profile --stats true --output ${LOG_FILENAME} --sample none --cpuctxsw none \
 python3 -m oneflow.distributed.launch \
---nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT \
-tools/train_net.py \
---config-file $CONFIG \
-model.cfg.hidden_dropout_prob=$hidden_dropout_prob \
-model.cfg.attention_probs_dropout_prob=$attention_probs_dropout_prob \
-model.cfg.bias_dropout_fusion=$bias_dropout_fusion \
-model.cfg.hidden_layers=$NUM_LAYER \
-model.cfg.hidden_size=$HIDDEN_SIZE \
-model.cfg.num_attention_heads=$NUM_ATT_HEADS \
-model.cfg.intermediate_size=$INTERMEDIATE_SIZE \
-model.cfg.ffn_hidden_size=$INTERMEDIATE_SIZE \
-model.cfg.head_size=$HEAD_SIZE \
-graph.enabled=$GRAPH_ENABLED \
-train.dist.pipeline_num_layers=$NUM_LAYER \
-train.train_micro_batch_size=$MICRO_BATCH_SIZE \
-train.global_batch_size=$GLOBAL_BATCH_SIZE \
-train.dist.tensor_parallel_size=$MP \
-train.dist.pipeline_parallel_size=$PP \
-train.amp.enabled=$USE_FP16 \
-train.activation_checkpoint.enabled=$ACTIVATION_CHECKPOINT \
-train.num_accumulation_steps=$ACC \
-train.evaluation.enabled=$EVALUATION_ENABLED \
-train.train_iter=$TRAIN_ITERS \
-train.train_epoch=$TRAIN_EPOCH \
-train.log_period=$LOG_PERIOD \
-train.zero_optimization.enabled=$ZERO_ENABLE \
-train.zero_optimization.stage=$ZERO_STAGE \
-train.load_weight=$LOAD_WEIGHT \
-train.output_dir=$LOG_FILENAME 2>&1 | tee ${LOG_FILENAME}/output.log
+    --nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT \
+    tools/train_net.py \
+    --config-file $CONFIG \
+    model.cfg.hidden_dropout_prob=$hidden_dropout_prob \
+    model.cfg.attention_probs_dropout_prob=$attention_probs_dropout_prob \
+    model.cfg.bias_dropout_fusion=$bias_dropout_fusion \
+    model.cfg.hidden_layers=$NUM_LAYER \
+    model.cfg.hidden_size=$HIDDEN_SIZE \
+    model.cfg.num_attention_heads=$NUM_ATT_HEADS \
+    model.cfg.intermediate_size=$INTERMEDIATE_SIZE \
+    model.cfg.ffn_hidden_size=$INTERMEDIATE_SIZE \
+    model.cfg.head_size=$HEAD_SIZE \
+    graph.enabled=$GRAPH_ENABLED \
+    train.dist.pipeline_num_layers=$NUM_LAYER \
+    train.train_micro_batch_size=$MICRO_BATCH_SIZE \
+    train.global_batch_size=$GLOBAL_BATCH_SIZE \
+    train.dist.tensor_parallel_size=$MP \
+    train.dist.pipeline_parallel_size=$PP \
+    train.amp.enabled=$USE_FP16 \
+    train.activation_checkpoint.enabled=$ACTIVATION_CHECKPOINT \
+    train.num_accumulation_steps=$ACC \
+    train.evaluation.enabled=$EVALUATION_ENABLED \
+    train.train_iter=$TRAIN_ITERS \
+    train.train_epoch=$TRAIN_EPOCH \
+    train.log_period=$LOG_PERIOD \
+    train.zero_optimization.enabled=$ZERO_ENABLE \
+    train.zero_optimization.stage=$ZERO_STAGE \
+    train.load_weight=$LOAD_WEIGHT \
+    train.output_dir=$LOG_FILENAME 2>&1 | tee ${LOG_FILENAME}/output.log
 
 ONEFLOW_VERSION=$(python3 -c 'import oneflow; print(oneflow.__version__)')
 ONEFLOW_LIBAI_COMMIT=$(git log --pretty=format:"%H" -n 1)
-echo "oneflow-version(git_commit)=$ONEFLOW_VERSION" >> ${LOG_FILENAME}/output.log
-echo "oneflow-commit(git_commit)=$ONEFLOW_COMMIT" >> ${LOG_FILENAME}/output.log
-echo "oneflow-libai(git_commit)=$ONEFLOW_LIBAI_COMMIT" >> ${LOG_FILENAME}/output.log
+echo "oneflow-version(git_commit)=$ONEFLOW_VERSION" >>${LOG_FILENAME}/output.log
+echo "oneflow-commit(git_commit)=$ONEFLOW_COMMIT" >>${LOG_FILENAME}/output.log
+echo "oneflow-libai(git_commit)=$ONEFLOW_LIBAI_COMMIT" >>${LOG_FILENAME}/output.log
 
 git checkout ./libai/engine/*.py
 git checkout ./libai/data/build.py
