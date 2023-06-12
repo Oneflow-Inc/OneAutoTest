8cef375c2467afe3535d457e56df0a6ce633f408
jinfengSUN <sjf1998112@gmail.com>
2023-06-12 11:33:13 +0800

    add env

--- onebench/libai/arg_train_platform.sh
+++ onebench/libai/arg_train_platform.sh
@@ -34,6 +34,10 @@ if [ $PLATFORM -eq 0 ]; then
     # 安装 TCCL 之后不需要 NCCL TOPO 文件
     # export NCCL_TOPO_FILE=/data_turbo/home/workspace/nccl-tests/nccl_topo_a800_1.6t.xml
     export NCCL_IB_QPS_PER_CONNECTION=4
+    export ONEFLOW_COMM_NET_IB_GID_INDEX=3
+    #export ONEFLOW_COMM_NET_IB_HCA=$NCCL_IB_HCA
+    export ONEFLOW_COMM_NET_IB_HCA=mlx5_bond_1:1
+
 fi
 
 if [ $PLATFORM -eq 1 ]; then
