b1838e82f320ba35fe4181529fbe24b4a1767c2a
jinfengSUN <sjf1998112@gmail.com>
2023-06-14 16:24:49 +0800

    Update run_subject_5.sh

--- onebench/libai/run_subject_5.sh
+++ onebench/libai/run_subject_5.sh
@@ -4,7 +4,7 @@ set -x
 python3 -m pip uninstall -y oneflow
 python3 -m pip install --find-links https://release.oneflow.info oneflow==0.9.0+cu117
 
-#数据集
+# 下载数据集
 wget -nc https://oneflow-test.oss-cn-beijing.aliyuncs.com/OneFlowAutoTest/libai/dataset/gpt2-vocab.json -P ./data_test/gpt_data
 wget -nc https://oneflow-test.oss-cn-beijing.aliyuncs.com/OneFlowAutoTest/libai/dataset/gpt2-merges.txt -P ./data_test/gpt_data
 wget -nc https://oneflow-test.oss-cn-beijing.aliyuncs.com/OneFlowAutoTest/libai/dataset/loss_compara_content_sentence.bin -P ./data_test/gpt_data
@@ -13,16 +13,14 @@ wget -nc https://oneflow-test.oss-cn-beijing.aliyuncs.com/OneFlowAutoTest/libai/
 wget -nc https://oneflow-test.oss-cn-beijing.aliyuncs.com/OneFlowAutoTest/libai/dataset/loss_compara_content_sentence.bin -P ./data_test/bert_data
 wget -nc https://oneflow-test.oss-cn-beijing.aliyuncs.com/OneFlowAutoTest/libai/dataset/loss_compara_content_sentence.idx -P ./data_test/bert_data
 
-# 脚本
+# 下载脚本
 wget -nc https://raw.githubusercontent.com/Oneflow-Inc/OneAutoTest/main/onebench/libai/args_train.sh -P ./
-wget -nc https://raw.githubusercontent.com/Oneflow-Inc/OneAutoTest/main/onebench/libai/extract_result.py -P ./
+wget -nc https://raw.githubusercontent.com/Oneflow-Inc/OneAutoTest/Subject_5/onebench/libai/extract_result.py -P ./
 
-#每次从0开始
-rm -rf ./libai
+#每次重新 clone
+rm -rf libai
 
-if [ ! -d "./libai" ]; then
-    git clone -b main --depth 1 https://github.com/Oneflow-Inc/libai.git
-fi
+git clone -b main --depth 1 https://github.com/Oneflow-Inc/libai.git
 
 cp -r ./data_test ./libai/
 cp ./args_train.sh ./libai/tools
